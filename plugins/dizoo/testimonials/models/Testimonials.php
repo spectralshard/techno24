44456e3f418355b5f1e8cbc941976a68975e7f01
alexandrefoisy <alexfoisy@me.com>
2019-10-28 20:17:50 -0400

    Extended Plugin for translation

--- plugins/dizoo/testimonials/models/Testimonials.php
+++ plugins/dizoo/testimonials/models/Testimonials.php
@@ -32,4 +32,10 @@ class Testimonials extends Model
         'name' => 'required',
         'quote' => 'required'
     ];
+
+    /**
+     * Extented for translation
+     */
+    public $implement = ['RainLab.Translate.Behaviors.TranslatableModel'];
+    public $translatable = ['name','quote'];
 }
