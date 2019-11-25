80fe5c5a47ae3a5c3bc43865f230479387f4c9a1
alexandrefoisy <alexfoisy@me.com>
2019-11-25 14:27:10 -0500

    Update Share codes

--- storage/framework/classes.php
+++ storage/framework/classes.php
@@ -172,4 +172,10 @@
   'Arcane\\Seo\\Classes\\Minifier' => 'plugins/arcane/seo/classes/Minifier.php',
   'Arcane\\Seo\\Classes\\Helper' => 'plugins/arcane/seo/classes/Helper.php',
   'Arcane\\Seo\\Classes\\Schema' => 'plugins/arcane/seo/classes/Schema.php',
+  'panakour\\backup\\Plugin' => 'plugins/panakour/backup/Plugin.php',
+  'Panakour\\Backup\\Models\\Settings' => 'plugins/panakour/backup/models/Settings.php',
+  'PanaKour\\Backup\\DropboxServiceProvider' => 'plugins/panakour/backup/DropboxServiceProvider.php',
+  'panakour\\backup\\Controllers\\backups' => 'plugins/panakour/backup/controllers/backups.php',
+  'PanaKour\\Backup\\Repository' => 'plugins/panakour/backup/Repository.php',
+  'PanaKour\\Backup\\Dropbox' => 'plugins/panakour/backup/Dropbox.php',
 );
