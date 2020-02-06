079e02d612af8b72bc79418a155c5c1d3db19546
alexandrefoisy <alexfoisy@me.com>
2020-02-06 15:05:54 -0500

    Update CMS + add links to webmails in nav

--- plugins/janvince/smallcontactform/vendor/composer/autoload_static.php
+++ plugins/janvince/smallcontactform/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit9b7ddbf724d4f592798ecc4665250702
+class ComposerStaticInit4aeb01b46bb8561458bca06d8da49345
 {
     public static $prefixLengthsPsr4 = array (
         'C' => 
@@ -23,8 +23,8 @@ class ComposerStaticInit9b7ddbf724d4f592798ecc4665250702
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit9b7ddbf724d4f592798ecc4665250702::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit9b7ddbf724d4f592798ecc4665250702::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit4aeb01b46bb8561458bca06d8da49345::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit4aeb01b46bb8561458bca06d8da49345::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
