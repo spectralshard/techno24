8744bb9e9db905730c0b13a87e3ca28111a20dc4
alexandrefoisy <alexfoisy@me.com>
2020-04-15 10:02:12 -0400

    Patch to have fixed on top when scrolling nav

--- themes/techno24/assets/js/techno24.js
+++ themes/techno24/assets/js/techno24.js
@@ -19,3 +19,21 @@ new WOW().init();
     };
 } )();
 function ml_webform_success_1608414(){var r=ml_jQuery||jQuery;r(".ml-subscribe-form-1608414 .row-success").show(),r(".ml-subscribe-form-1608414 .row-form").hide()}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("fixed-top")
+    } else {
+        navbar.classList.remove("fixed-top");
+    }
+}
